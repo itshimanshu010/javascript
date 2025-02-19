74a6cb4873671f4fa5f211d4f10cac87ac2cce6d
Himanshu Jaiswal <himanshu.mau1999@gmail.com>
2025-02-19 11:15:14 +0530

    DataType and Typeof

---
+++ 01_Basic/02_DataTypes.js
@@ -0,0 +1,12 @@
+let name = "Himanshu";
+let age = 25;
+let isActive = true;
+
+//Typeof = it used to data type of the value or variable
+console.log(typeof 1);
+console.log(typeof age);
+console.log(typeof "Hima");
+console.log(typeof undefined);
+console.log(typeof null); //object
+
+
