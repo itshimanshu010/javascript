7884da599956bdcf1f08dd42a493369b78f4acb2
Himanshu Jaiswal <himanshu.mau1999@gmail.com>
2025-02-19 10:58:09 +0530

    Practice

---
+++ 01_Basic/01_Variables.js
@@ -0,0 +1,16 @@
+const account_Id = 7852
+let account_mail = "himanshu@gmail.com"
+var account_password = "789456321"
+let account_state
+
+accountCity = "Noida"
+
+// account_Id = 1234 //not allowed because it is const
+
+account_mail="him@gmail.com"
+
+console.log(account_Id)
+console.log(account_mail)
+
+//console table
+console.table([account_Id,account_mail,account_password,accountCity,account_state]);
